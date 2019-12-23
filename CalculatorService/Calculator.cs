49bcf3675828df13fda6fb69fb267608800bb12c
Ray Hsu <rayhsu02@yahoo.com>
2019-12-23 15:24:32 -0800

    added tests

--- CalculatorService/Calculator.cs
+++ CalculatorService/Calculator.cs
@@ -4,5 +4,20 @@ namespace calculatorService
 {
     public class Calculator
     {
+        public int Add(string inputStr)
+        {
+
+            string[] numbers = inputStr.Split(',');
+            int total = 0;
+
+            foreach (var num in numbers)
+            {
+                int i = 0;
+                bool result = int.TryParse(num, out i);
+                total += i;
+            }
+
+            return total;
+        }
     }
 }
--- calculator.tests/CalculatorTests.cs
+++ calculator.tests/CalculatorTests.cs
@@ -1,16 +1,100 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using calculatorService;
 
-namespace calculator.tests {
+namespace calculator.tests
+{
     [TestClass]
-    public class CalculatorTests {
+    public class CalculatorTests
+    {
         private Calculator _calculator;
 
-        public CalculatorTests () {
+        public CalculatorTests()
+        {
             _calculator = new Calculator();
         }
 
         [TestMethod]
-        public void TestMethod1 () { }
+        public void Add_20_Return20()
+        {
+            //Arrange
+            string input = "20";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 20;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Add_1_plus_500_Return501()
+        {
+            //Arrange
+            string input = "1,500";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 501;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Add_4_minus_3_Return1()
+        {
+            //Arrange
+            string input = "4,-3";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 1;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Add_EmptyInput()
+        {
+            //Arrange
+            string input = "";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 0;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Add_MissingNumbersInput()
+        {
+            //Arrange
+            string input = "100,";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 100;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Add_InvalidNumbersInput_Return5()
+        {
+            //Arrange
+            string input = "5,tyty";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 5;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
     }
 }
