0827007ca6ac0ab7ba8a8de97d92d9354ee71f87
Ray Hsu <rayhsu02@yahoo.com>
2019-12-24 12:09:33 -0800

    custom delimiters

--- CalculatorService/Calculator.cs
+++ CalculatorService/Calculator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Text;
 
 namespace calculatorService
 {
@@ -6,7 +8,27 @@ namespace calculatorService
     {
         public int Add(string inputStr)
         {
-            string delimiter  = "\n";
+            bool HasCustomDelimiter = inputStr.Contains("//");
+            bool HasMultiCustDelimiter = inputStr.Contains("[");
+            bool IsSingleCharDelimiter = HasCustomDelimiter == true && HasMultiCustDelimiter == false;
+
+            if (HasCustomDelimiter)
+            {
+                inputStr = inputStr.Replace("//", "");
+                string[] delimiters = GetDeliomiters(inputStr);
+                foreach(string s in delimiters)
+                {
+                    inputStr = inputStr.Replace(s, ",");
+                }
+            }
+           
+            if (IsSingleCharDelimiter)
+            {
+                char delimiter = inputStr[0];
+                inputStr = inputStr.Replace(delimiter, ',');
+            }
+            //helper function to get delimiters
+
             inputStr = inputStr.Replace('\n', ',');
             string[] numbers = inputStr.Split(',');
             int total = 0;
@@ -15,10 +37,41 @@ namespace calculatorService
             {
                 int i = 0;
                 bool result = int.TryParse(num, out i);
+
+                if (i < 0) throw new Exception("negtive number expection");
+                if (i > 1000) i = 0;
+
                 total += i;
             }
 
             return total;
+        }
+
+        private string[] GetDeliomiters(string inputStr)
+        {
+            ArrayList list = new ArrayList();
+            Stack stack = new Stack();
+            StringBuilder sb = new StringBuilder();
+
+            foreach(char c in inputStr)
+            {
+                if (c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ']')
+                {
+                    stack.Pop();
+                    list.Add(sb.ToString());
+                    sb.Clear();
+                }
+                else
+                {
+                    if(stack.Count != 0) sb.Append(c);
+                }
+            }
+
+            return (string[])list.ToArray(typeof(string));
         }
     }
 }
--- calculator.tests/CalculatorTests.cs
+++ calculator.tests/CalculatorTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using calculatorService;
+using System;
 
 namespace calculator.tests
 {
@@ -120,6 +121,91 @@ namespace calculator.tests
             //Act
             var result = _calculator.Add(input);
             int expect = 6;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Add_DenyNegativeNumbers_ReturnException()
+        {
+            //Arrange
+            string input = "-1,-3,5";
+
+            //Act
+            //var result = _calculator.Add(input);
+            //int expect = 0;
+
+            //Assert
+            Assert.ThrowsException<Exception>(() => _calculator.Add(input));
+          
+        }
+
+        [TestMethod]
+        public void Add_GranterThan1000_Return8()
+        {
+            //Arrange
+            string input = "2,1001,6";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 8;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Add_SingleCustomDelimeter_Return102()
+        {
+            //Arrange
+            string input = "//,\n2,ff,100";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 102;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Add_SingleCustomDelimeter_Return7()
+        {
+            //Arrange
+            string input = "//#\n2#5";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 7;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Add_AnyLengthCustomDelimeter_Return66()
+        {
+            //Arrange
+            string input = "//[***]\n11***22***33";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 66;
+
+            //Assert
+            Assert.AreEqual(expect, result);
+        }
+
+        [TestMethod]
+        public void Add_MultiCustomDelimeter_Return110()
+        {
+            //Arrange
+            string input = "//[*][!!][r9r]\n11r9r22*hh*33!!44";
+
+            //Act
+            var result = _calculator.Add(input);
+            int expect = 110;
 
             //Assert
             Assert.AreEqual(expect, result);
